cfa1393b745e3cceaa1d3a3b0ba1d2de8686060b
rishabh.kamath <rshbhk22@gmail.com>
2023-02-01 01:53:02 -0600

    added handlers for install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,26 @@
 const butInstall = document.getElementById('buttonInstall');
-
+let deferredPrompt;
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {  
+    event.preventDefault();
+    deferredPrompt = event;
+    butInstall.style.display = 'block';});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    butInstall.style.display = 'none';
+    deferredPrompt.prompt();
+    const outcome = await deferredPrompt.userChoice;
+    if (outcome.outcome === 'accepted') {
+      console.log('PWA installation accepted');
+    } else {
+      console.log('PWA installation rejected');
+    }
+    deferredPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('PWA installed');
+});
