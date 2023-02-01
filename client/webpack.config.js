6829489582daddbe6e61ee1852bce17ea6f10f90
rishabh.kamath <rshbhk22@gmail.com>
2023-02-01 01:22:45 -0600

    added CSS and babel loaders to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = () => {
       //generate html file
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'JATE'
+        title: 'JATE',
         favicon: './favicon.ico'
       }),
 
@@ -30,11 +30,48 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'service-worker.js',
       }),
+
+      //creating manifest
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        fingerprint: false,
+        inject: true,
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      })
     ],
 
     module: {
       rules: [
-        
+        //CSS loader
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        //babel loader
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
